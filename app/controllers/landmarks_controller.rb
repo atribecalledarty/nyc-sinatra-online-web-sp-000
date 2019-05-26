3b6f0a0e9334cf7e9ecfa31d0975c81e2fbcd7d0
Arthur Song <arth3rs0ng@gmail.com>
2019-05-26 07:05:13 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -33,7 +33,6 @@ class LandmarksController < ApplicationController
     @landmark.name = params["landmark"]["name"]
     @landmark.year_completed = params["landmark"]["year_completed"]
     @landmark.save
-    binding.pry
     
     redirect "landmarks/#{@landmark.id}"
   end
