aed69611561b6c07b8b865486edb709e0ccaac7b
Arthur Song <arth3rs0ng@gmail.com>
2019-05-26 05:30:23 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,4 +5,10 @@ class FiguresController < ApplicationController
     
     erb :'figures/index'
   end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    
+    erb :'figures/show'
+  end
 end
